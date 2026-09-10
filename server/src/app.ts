0d2bb1d98242f5d667838ee1c7561b3826be0cf9
arifucoder <arifucoder@gmail.com>
2026-09-10 10:52:19 +0600

    feat: add server essential packages and bootstraping code

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test route
+app.get("/", (req: Request, res: Response) => {
+	res.json({ message: "Server is running" });
+});
+
+// Routes (পরে এখানে import করবা)
+// app.use("/api/todos", todoRoutes);
+
+export default app;
