4fe915e978955f64061a6869f881eac5ce43cda7
arifucoder <arifucoder@gmail.com>
2026-09-13 07:23:24 +0600

    chor: add heading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -102,7 +102,7 @@ function App() {
 			<div className="max-w-4xl my-5 mx-auto">
 				{/* Add / Edit Form */}
 				<div>
-					<h1>Add Todos</h1>
+					<h1>Add Task</h1>
 				</div>
 				<div className="flex gap-2 mb-4">
 					<input
